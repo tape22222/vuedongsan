0ff17f97e820d2b0d11607cc8cd2e766c24572ab
tape22222 <xogml4435@naver.com>
2022-11-07 17:43:55 +0900

    2day

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="black_bg" v-if="openModal">
+    <div class="white_bg">
+      <h4>상세 페이지</h4>
+      <p>상세 페이지 내용</p>
+      <button @click="openModal = false">닫기</button>
+    </div>
+  </div>
+  <div class="menu">
+    <a v-for="a in menudata" :key="a">{{a}}</a>
+  </div>
+  <!--<div v-for="(a, i) in products" :key="i">
+    <img src="./assets/room0.jpg" class="room_img">
+    <h4>{{products[i]}}</h4> or <h4>{{a}}</h4>
+    <p>{{i + 5}}0 만원</p>
+    <button @click="report[i]++">허위매물 신고</button>
+    <span> 신고수: {{report[i]}}</span>
+  </div>-->
   <div>
-    <h4>{{products[0]}}</h4>
+    <img src="./assets/room0.jpg" class="room_img">
+    <h4 @click="openModal = true">{{products[0]}}</h4>
     <p>50 만원</p>
+    <button @click="report[0]++">허위매물 신고</button>
+    <span> 신고수: {{report[0]}}</span>
   </div>
   <div>
+    <img src="./assets/room1.jpg" class="room_img">
     <h4>{{products[1]}}</h4>
-    <p>50 만원</p>
+    <p>60 만원</p>
+    <button @click="report[1]++">허위매물 신고</button>
+    <span> 신고수: {{report[1]}}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" class="room_img">
     <h4>{{products[2]}}</h4>
-    <p>50 만원</p>
+    <p>70 만원</p>
+    <button @click="report[2]++">허위매물 신고</button>
+    <span> 신고수: {{report[2]}}</span>
   </div>
 </template>
 
@@ -20,8 +45,14 @@ export default {
   
   data() {
     return {
+      openModal : false,
+      menudata : ['Home', 'Shop', 'About'],
+      report : [0, 0, 0],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
+  },
+
+  methods : {
   },
 
   components: {
@@ -30,12 +61,51 @@ export default {
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin : 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black_bg {
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+}
+
+.white_bg {
+  widows: 100%;
+  padding: 20px;
+  border-radius: 8px;
+  background: white;
+}
+
+.menu {
+  padding: 15px;
+  border-radius: 5px;
+  background: darkslateblue;
+}
+
+.menu a {
+  padding: 10px;
+  color: white;
+}
+
+.room_img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
